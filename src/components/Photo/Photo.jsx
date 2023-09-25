c87798eafe4e14b5998988332af4a52730a11fb5
Yogesh More <135803626+iamyogeshmore@users.noreply.github.com>
2023-09-26 00:15:29 +0530

    Added: validation for extracting words from discription.

--- src/components/Photo/Photo.jsx
+++ src/components/Photo/Photo.jsx
@@ -6,15 +6,40 @@ function Photo({ photo, onWordExtracted }) {
   const [extractedWord, setExtractedWord] = useState([]);
 
   const extractWord = () => {
-    if (photo.description && startingPoint !== "" && endPoint !== "") {
-      const words = photo.description
-        .split(" ")
-        .slice(startingPoint - 1, endPoint)
-        .join(" ");
-      setExtractedWord([words]);
-      console.log(words);
-      onWordExtracted(words);
+    if (!photo.description) {
+      console.error("No description available.");
+      alert("No description available.");
+      return;
     }
+
+    const descriptionWords = photo.description.split(/\s+/);
+    const start = parseInt(startingPoint);
+    const end = parseInt(endPoint);
+
+    if (start < 1 || end < 1) {
+      console.error("Starting and ending points must be positive.");
+      alert("Starting and ending points must be positive.");
+      return;
+    }
+
+    if (end > descriptionWords.length) {
+      console.log(descriptionWords);
+      console.error("End point exceeds the total number of words in the description.");
+      alert("End point exceeds the total number of words in the description.");
+      return;
+      console.log(descriptionWords);
+    }
+
+    if (end < start) {
+      console.error("Ending point must be greater than or equal to the starting point.");
+      alert("Ending point must be greater than or equal to the starting point.");
+      return;
+    }
+
+    const wordsToExtract = descriptionWords.slice(start - 1, end).join(" ");
+    setExtractedWord([wordsToExtract]);
+    console.log(wordsToExtract);
+    onWordExtracted(wordsToExtract);
   };
 
   return (
@@ -35,7 +60,7 @@ function Photo({ photo, onWordExtracted }) {
           value={endPoint}
           onChange={(e) => setEndPoint(e.target.value)}
         />
-        <button onClick={extractWord}> words</button>
+        <button onClick={extractWord}> Extract words</button>
       </div>
     </div>
   );
